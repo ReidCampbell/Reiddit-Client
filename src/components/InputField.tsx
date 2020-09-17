e108e1aa844d90c8ea29f5c931a54bf2289872f2
ReidCampbell <reidscottcampbell@gmail.com>
2020-09-17 16:56:45 +0100

    input field component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,42 @@
+import React, { InputHTMLAttributes } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Textarea,
+} from '@chakra-ui/core';
+import { useField } from 'formik';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+  textarea?: boolean;
+};
+
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  textarea,
+  size,
+  ...props
+}) => {
+  let InputOrTextarea = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  }
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <InputOrTextarea
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
+};
+
+export default InputField;
