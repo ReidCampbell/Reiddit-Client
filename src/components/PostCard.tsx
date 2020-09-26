ebd028dbec914971d45eaafce95813c927818862
ReidCampbell <reidscottcampbell@gmail.com>
2020-09-26 16:36:28 +0100

    Add styling to PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,17 +1,22 @@
-import { Text, Flex, Box, Link, Heading } from '@chakra-ui/core';
+import { Text, Flex, Box, Link, Heading, Avatar } from '@chakra-ui/core';
 import React from 'react';
-import { EditDeletePostButtons } from './EditDeletePostButtons';
 import UpdootSection from './UpdootSection';
 import NextLink from 'next/link';
 import { Post } from '../generated/graphql';
+import { Divider } from '@chakra-ui/core';
+import dayjs from 'dayjs';
 
 interface PostCardProps {
   post: Post;
 }
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  const now = dayjs();
+  const postTime = dayjs(parseInt(post.createdAt));
+  let timeSincePost = now.diff(postTime, 'day');
+
   return (
-    <Flex backgroundColor='white' mb={45} p={5} shadow='lg' borderRadius='12px'>
+    <Flex backgroundColor='white' mt={45} p={5} shadow='lg' borderRadius='12px'>
       <UpdootSection post={post} />
       <Box ml={4} flex={1}>
         <NextLink href='/post/[id]' as={`/post/${post.id}`}>
@@ -25,9 +30,21 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
           <Text color='#959595' flex={1} mt={4}>
             {post.textSnippet}
           </Text>
-          <Box ml='auto'>
-            <EditDeletePostButtons id={post.id} creatorId={post.creator.id} />
-          </Box>
+        </Flex>
+        <Divider mt={4} borderColor='#aaaaaa' />
+        <Flex align='center' mt={4}>
+          <Avatar size='xs' mr={4} />
+          <Flex align='baseline'>
+            <Text fontSize='sm' color='#aaaaaa' mr={15}>
+              Posted by{' '}
+              <Box as='span' color='#3d5af1' fontSize='sm'>
+                {post.creator.username}
+              </Box>
+            </Text>
+            <Text fontSize='xs' color='#aaaaaa'>
+              {timeSincePost} days ago
+            </Text>
+          </Flex>
         </Flex>
       </Box>
     </Flex>
