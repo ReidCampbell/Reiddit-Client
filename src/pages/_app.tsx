a3f3c6fa10aaad64ebf186fa7cda362639ba7e4d
ReidCampbell <reidscottcampbell@gmail.com>
2020-09-27 17:50:57 +0100

    Added global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ColorModeProvider, CSSReset, ThemeProvider } from '@chakra-ui/core';
 import theme from '../theme';
+import '../index.css';
 
 function MyApp({ Component, pageProps }: any) {
   return (
