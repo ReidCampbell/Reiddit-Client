e351d11f1163f91e7aa51a1aba51aad556ebd235
ReidCampbell <reidscottcampbell@gmail.com>
2020-10-02 22:00:55 +0100

    Update single post page view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { withApollo } from '../utils/withApollo';
 import PostCard from '../components/PostCard';
 
 const Index = () => {
-  const { data, loading, fetchMore, variables } = usePostsQuery({
+  const { data, loading, error, fetchMore, variables } = usePostsQuery({
     variables: {
       limit: 15,
       cursor: null,
@@ -16,7 +16,7 @@ const Index = () => {
   });
 
   if (!loading && !data) {
-    return <div>Your query failed :</div>;
+    return <div>Your query failed : {error?.message}</div>;
   }
 
   return (
@@ -39,32 +39,8 @@ const Index = () => {
         <div>loading</div>
       ) : (
         <Stack>
-          {data!.posts.posts.map(p =>
-            !p ? null : (
-              // <Flex key={p.id} p={5} shadow='sm' borderWidth='1px'>
-              //   <UpdootSection post={p} />
-              //   <Box ml={4} flex={1}>
-              //     <NextLink href='/post/[id]' as={`/post/${p.id}`}>
-              //       <Link ml='auto'>
-              //         <Heading fontSize='xl'>{p.title}</Heading>
-              //       </Link>
-              //     </NextLink>
-              //     <Text>{p.creator.username}</Text>
-              //     <Flex align='center'>
-              //       <Text flex={1} mt={4}>
-              //         {p.textSnippet}
-              //       </Text>
-              //       <Box ml='auto'>
-              //         <EditDeletePostButtons
-              //           id={p.id}
-              //           creatorId={p.creator.id}
-              //         />
-              //       </Box>
-              //     </Flex>
-              //   </Box>
-              // </Flex>
-              <PostCard key={p.id} post={p} />
-            )
+          {data!.posts.posts.map(post =>
+            !post ? null : <PostCard key={post.id} post={post} />
           )}
         </Stack>
       )}
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-import { Box, Heading } from '@chakra-ui/core';
+import { Box, Button, Heading } from '@chakra-ui/core';
 import { useGetPostFromUrl } from '../../utils/useGetPostsFromUrl';
 import { EditDeletePostButtons } from '../../components/EditDeletePostButtons';
 import { withApollo } from '../../utils/withApollo';
+import {
+  useCreateCommentMutation,
+  PostDocument,
+} from '../../generated/graphql';
+import { Formik, Form } from 'formik';
+import InputField from '../../components/InputField';
+import { useRouter } from 'next/router';
+import { useGetIntId } from '../../utils/useGetIntId';
+import CommentCard from '../../components/CommentCard';
+import PostCard from '../../components/PostCard';
 
 const Post: React.FC = ({}) => {
   const { data, loading } = useGetPostFromUrl();
+
+  const [createComment] = useCreateCommentMutation();
+  const router = useRouter();
+  const intId = useGetIntId();
 
   if (loading) {
     return (
@@ -26,14 +40,54 @@ const Post: React.FC = ({}) => {
 
   return (
     <Layout>
-      <Heading mb={4}>{data.post.title}</Heading>
-      <Box mb={4}>
-        {data.post.text}
-        <EditDeletePostButtons
-          id={data.post.id}
-          creatorId={data.post.creator.id}
-        />
-      </Box>
+      <PostCard post={data.post}>
+        {data.post.comments &&
+          data.post.comments.map((comment: any) => {
+            return <CommentCard key={comment.id} comment={comment} />;
+          })}
+      </PostCard>
+
+      <Formik
+        initialValues={{ text: '', postId: null }}
+        onSubmit={async (values, { resetForm }) => {
+          const { errors } = await createComment({
+            variables: { ...values, postId: intId },
+            update: cache => {
+              cache.evict({ fieldName: 'comments:{}' });
+            },
+            refetchQueries: [
+              {
+                query: PostDocument,
+                variables: {
+                  id: intId,
+                },
+              },
+            ],
+          });
+          if (!errors) {
+            resetForm();
+            router.push({
+              pathname: `/post/${intId}`,
+            });
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Box mt={4}>
+              <InputField
+                textarea
+                name='text'
+                placeholder='text...'
+                label='Body'
+              />
+            </Box>
+            <Button mt={4} type='submit' isLoading={isSubmitting}>
+              Create Comment
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Layout>
   );
 };
