49552f216fcd358ae804e4e369bf2be949c89ef1
ReidCampbell <reidscottcampbell@gmail.com>
2020-10-21 19:28:58 +0100

    Update post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Layout from '../../components/Layout';
-import { Box, Button, Heading } from '@chakra-ui/core';
+import Container from '../../components/Container';
+import { Box, Button } from '@chakra-ui/core';
 import { useGetPostFromUrl } from '../../utils/useGetPostsFromUrl';
-import { EditDeletePostButtons } from '../../components/EditDeletePostButtons';
 import { withApollo } from '../../utils/withApollo';
 import {
   useCreateCommentMutation,
@@ -18,77 +17,31 @@ import PostCard from '../../components/PostCard';
 const Post: React.FC = ({}) => {
   const { data, loading } = useGetPostFromUrl();
 
-  const [createComment] = useCreateCommentMutation();
-  const router = useRouter();
-  const intId = useGetIntId();
-
   if (loading) {
     return (
-      <Layout>
+      <Container>
         <div>loading...</div>
-      </Layout>
+      </Container>
     );
   }
 
   if (!data?.post) {
     return (
-      <Layout>
+      <Container>
         <Box>Could not find post</Box>
-      </Layout>
+      </Container>
     );
   }
 
   return (
-    <Layout>
-      <PostCard post={data.post}>
+    <Container>
+      <PostCard post={data.post} postIsOpen>
         {data.post.comments &&
           data.post.comments.map((comment: any) => {
             return <CommentCard key={comment.id} comment={comment} />;
           })}
       </PostCard>
-
-      <Formik
-        initialValues={{ text: '', postId: null }}
-        onSubmit={async (values, { resetForm }) => {
-          const { errors } = await createComment({
-            variables: { ...values, postId: intId },
-            update: cache => {
-              cache.evict({ fieldName: 'comments:{}' });
-            },
-            refetchQueries: [
-              {
-                query: PostDocument,
-                variables: {
-                  id: intId,
-                },
-              },
-            ],
-          });
-          if (!errors) {
-            resetForm();
-            router.push({
-              pathname: `/post/${intId}`,
-            });
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Box mt={4}>
-              <InputField
-                textarea
-                name='text'
-                placeholder='text...'
-                label='Body'
-              />
-            </Box>
-            <Button mt={4} type='submit' isLoading={isSubmitting}>
-              Create Comment
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Layout>
+    </Container>
   );
 };
 
