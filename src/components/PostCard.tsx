0628d42067b6046f8e8f9a9ccd5d91f547153dba
ReidCampbell <reidscottcampbell@gmail.com>
2020-10-21 19:28:06 +0100

    Update PostCard styling

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,21 +1,52 @@
-import { Text, Flex, Box, Link, Heading, Avatar, Icon } from '@chakra-ui/core';
-import React from 'react';
+import {
+  Text,
+  Flex,
+  Box,
+  Link,
+  Heading,
+  Avatar,
+  Icon,
+  Button,
+} from '@chakra-ui/core';
+import React, { useState } from 'react';
 import UpdootSection from './UpdootSection';
 import NextLink from 'next/link';
 import { Divider } from '@chakra-ui/core';
 import dayjs from 'dayjs';
 import { useRouter } from 'next/router';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { EditDeletePostButtons } from './EditDeletePostButtons';
+import {
+  Post,
+  PostDocument,
+  useCreateCommentMutation,
+} from '../generated/graphql';
+import { Formik, Form } from 'formik';
+import InputField from './InputField';
+import { useGetIntId } from '../utils/useGetIntId';
+import CreateCommentForm from './CreateCommentForm';
 
-interface PostCardProps {
-  post: any;
+interface PostsCardProps {
+  post: Post;
+  postIsOpen: boolean;
 }
 
-const PostCard: React.FC<PostCardProps> = ({ post, children }) => {
+const PostsCard: React.FC<PostsCardProps> = ({
+  post,
+  postIsOpen,
+  children,
+}) => {
+  const [replyOpen, setReplyOpen] = useState(false);
+  const [createComment] = useCreateCommentMutation();
+  const router = useRouter();
+  const intId = useGetIntId();
+
   dayjs.extend(relativeTime);
   const postTime = dayjs().to(parseInt(post.createdAt));
 
   const calculateComments = () => {
+    if (!post.comments) return;
+
     let comments: number[] = [];
     post.comments.map((comment: any) => {
       comments.push(comment.replies.length);
@@ -23,8 +54,6 @@ const PostCard: React.FC<PostCardProps> = ({ post, children }) => {
 
     return comments.reduce((a, b) => a + b, 0) + post?.comments?.length;
   };
-
-  const router = useRouter();
 
   return (
     <Flex
@@ -42,13 +71,20 @@ const PostCard: React.FC<PostCardProps> = ({ post, children }) => {
     >
       <UpdootSection post={post} />
       <Box px='2rem' flex={1}>
-        <NextLink href='/post/[id]' as={`/post/${post.id}`}>
-          <Link ml='auto'>
-            <Heading color='#444444' fontSize='xl'>
-              {post.title}
-            </Heading>
-          </Link>
-        </NextLink>
+        <Flex justify='space-between'>
+          <NextLink href='/post/[id]' as={`/post/${post.id}`}>
+            <Link>
+              <Heading color='#444444' fontSize='xl'>
+                {post.title}
+              </Heading>
+            </Link>
+          </NextLink>
+          <Box>
+            {postIsOpen && (
+              <EditDeletePostButtons id={post.id} creatorId={post.creator.id} />
+            )}
+          </Box>
+        </Flex>
         <Flex align='center'>
           <Text color='#959595' flex={1} mt={4}>
             {post.text ? post.text : post.textSnippet}
@@ -57,7 +93,11 @@ const PostCard: React.FC<PostCardProps> = ({ post, children }) => {
         <Divider mt={4} borderColor='#aaaaaa' />
         <Flex mt={4} justify='space-between'>
           <Flex align='center'>
-            <Avatar size='xs' mr={4} />
+            <Avatar
+              size='xs'
+              mr={4}
+              src={post.creator.avatar ? post.creator.avatar : ''}
+            />
             <Text fontSize='sm' color='#aaaaaa' mr={15}>
               Posted by{' '}
               <Box as='span' color='#3d5af1' fontSize='sm'>
@@ -75,10 +115,26 @@ const PostCard: React.FC<PostCardProps> = ({ post, children }) => {
             </Text>
           </Flex>
         </Flex>
+        {postIsOpen && (
+          <Text
+            onClick={() => {
+              setReplyOpen(!replyOpen);
+            }}
+            fontSize='sm'
+            color='#aaaaaa'
+            cursor='pointer'
+            mt={2}
+          >
+            Reply
+          </Text>
+        )}
+        <Box>
+          {replyOpen && <CreateCommentForm setReplyOpen={setReplyOpen} />}
+        </Box>
         {children}
       </Box>
     </Flex>
   );
 };
 
-export default PostCard;
+export default PostsCard;
